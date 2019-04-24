c16997805e2ad739f3d8ab84f102dbf132f16c09
sunayanabali <47163867+sunayanabali@users.noreply.github.com>
2019-04-23 21:08:48 -0400

    Add files via upload

--- studio/exercises/scripts.js
+++ studio/exercises/scripts.js
@@ -11,10 +11,6 @@ function randomStuff(){
 var randomShape = shapes[ Math.floor(Math.random() * shapes.length ) ]
 var clone = randomShape.clone().appendTo('body');
 
-var colors = ["#739600", "#1F5EA8", "#F72C2C", "#FA5021", "#FB9F54", "#FFAD00,#FFE33B"]
-var rand = [Math.floor(Math.random() * colors.length)];
-$("body").css("background-color", colors[rand]);
-
 clone.last().css('left', Math.random() * width ) 
 clone.last().css('top', Math.random() * height )
 clone.last().css('opacity', Math.random() * 1 )
@@ -26,6 +22,6 @@ randomStuff();
 setInterval(function(){
 
 randomStuff();
-},90)
+},100)
 //end doucment ready function
 })
