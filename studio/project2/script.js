f1a8c094f3f8132ac87b42b1145fdd567c074136
sunayanabali <47163867+sunayanabali@users.noreply.github.com>
2019-04-25 19:13:00 -0400

    Add files via upload

--- studio/project2/script.js
+++ studio/project2/script.js
@@ -1,20 +1,18 @@
-window.onload = function () {
-var speed = 10, // the box will move by 10 pixels on every step
-direction = 1, // 1 = move right; -1 = move left
-boxElement = document.getElementById('box2');
+//check if document is ready
+$(document).ready(function(){
 
-if (boxElement) {
-    boxElement.addEventListener('mouseover', function () {
-        // Calculate and store some of the box coordinates:
-        var boxLeftPos = boxElement.offsetLeft,
-            boxRightPos = boxLeftPos + boxElement.offsetWidth;
-        // When right side of the box goes too far - change direction:
-        if (boxRightPos > document.body.offsetWidth) {
-            direction = -1;
-        }
-        // When left side of the box goes too far - change direction:
-        if (boxLeftPos < 0) {
-            direction = 1;
-        }
-        // Recalculate position:
-        boxElement.style.left = (boxLeftPos + speed * direction) + 'px';
+var colors = ["lightgreen", "lightbrown", "lightblue", "pink", "orange"]
+
+setInterval(function(){
+//create random number
+var randomColor = Math.floor(Math.random()*colors.length);
+
+console.log(randomColor)
+
+//change the css
+$(".box1").css("background-color", colors[randomColor])
+
+},950)
+//end document ready function
+})
+// add lines using javascript..
