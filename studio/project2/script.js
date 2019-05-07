21c4fba2c6f10d14ba598cf0d025ba946a17957f
sunayanabali <47163867+sunayanabali@users.noreply.github.com>
2019-05-07 17:34:41 -0400

    Add files via upload

--- studio/project2/script.js
+++ studio/project2/script.js
@@ -1,18 +1,20 @@
 //check if document is ready
 $(document).ready(function(){
 
-var colors = ["#ffff80", "#ff9999","#ffccff","#d9b3ff","#b3b3ff","#b3ffff","#b3ffb3","#ffcc99","#99ddff","#c6ffb3","#ffb3b3"]
-var colors2 = ["#b300b3", "#993366", "#ff0000", "#ff3300", "#ff9900","#99cc00","#009933","#008080","#007399","#003399","#330080"]
+var colors = ["#DEFFE2","#e6f7ff","#B6DAFE","#FFE6C8","#FEE6F0","#FFCBC5","#E6BEEE","#B7F1FF","#B5FDD0","#FFFAB6","#FFB6D5","#EFA0F5",
+"#ffff80", "#ff9999","#ffccff","#d9b3ff","#b3ffff","#ffcc99","#99ddff","#c6ffb3","#ffb3b3"]
+var colors2 = ["#0180F0","#0001F0","#7800F0","#CA01F0","#F001D1","#F0005B","#F04600","#ff0000","#F08F01","#006A14","#00306B","#3D006B","#6B0064","#6B0101",
+"#b300b3", "#993366","#ff3300", "#FFA400","#99cc00","##80ff00","#007399","#003399","#330080"]
 
 setInterval(function(){
 //create random number
-var randomColor = Math.floor(Math.random(2)*colors.length);
-var randomColor2 = Math.floor(Math.random(4)*colors2.length);
+var randomColor = Math.floor(Math.random(10)*colors.length);
+var randomColor2 = Math.floor(Math.random(20)*colors2.length);
 
 console.log(randomColor)
 
 //change the css
-$(".box").css("background-color", colors[randomColor])
+$(".mybox").css("background-color", colors[randomColor])
 $(".box1").css("background-color", colors[randomColor])
 $(".box2").css("background-color", colors[randomColor])
 $(".box3").css("background-color", colors[randomColor])
@@ -39,7 +41,7 @@ $(".box23").css("background-color", colors[randomColor])
 $(".box24").css("background-color", colors[randomColor])
 $(".box25").css("background-color", colors[randomColor])
 
-$(".box").css("border-color", colors2[randomColor2])
+$(".mybox").css("border-color", colors2[randomColor2])
 $(".box1").css("border-color", colors2[randomColor2])
 $(".box2").css("border-color", colors2[randomColor2])
 $(".box3").css("border-color", colors2[randomColor2])
