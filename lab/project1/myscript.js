44caa3fe653af27bcefb1e4ff0daa0490c77f4f3
sunayanabali <47163867+sunayanabali@users.noreply.github.com>
2019-05-08 00:57:55 -0400

    Add files via upload

--- lab/project1/myscript.js
+++ lab/project1/myscript.js
@@ -6,10 +6,7 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
-// close icon in modal
- let closeicon = document.querySelector(".close");
- // declare modal
- let modal = document.getElementById("popup1")
+let closeicon = document.querySelector(".close");
 
 function flipCard() {
   if (lockBoard) return;
@@ -28,10 +25,24 @@ function flipCard() {
   checkForMatch();
 }
 
+var matches = 0;
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  if(isMatch) {
+    disableCards();
 
-  isMatch ? disableCards() : unflipCards();
+  matches = matches + 1;
+
+    checkForWin();
+  } else {
+    unflipCards();
+  }
+}
+
+function checkForWin() {
+  if(matches == 6) {
+    congratulations()
+  }
 }
 
 function disableCards() {
@@ -59,20 +70,25 @@ function resetBoard() {
 
 (function shuffle() {
   cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
+    let randomPos = Math.floor(Math.random() * 15);
+   card.style.order = randomPos;
   });
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-})
-
+});
+let modal = document.querySelector("#popup1");
+console.log(modal);
 function congratulations(){
-    if (firstCard.dataset.framework === secondCard.dataset.framework;){
-        clearInterval(interval);
-        // show congratulations modal
-        modal.classList.add("show");
+  modal.classList.add("show");
+  closeModal();
+}
 
-}}
+function closeModal(){
+    closeicon.addEventListener("click", function(e){
+        modal.classList.remove("show");
+        startGame();
+    })
+}
 
 //how to link all cards true to a button appearing for 'level two' or next 'card set' or to activate it at the end.
